359bcabb76cb61f44cd6939d636e37a2d075bb6a
Buczko <kacper.buczko@capgemini.com>
2025-02-26 12:26:50 +0100

    Cleanup

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,12 +1,21 @@
+import {fetchFilteredCustomers} from "@/app/lib/data";
+import Table from "@/app/ui/customers/table";
 import {Metadata} from "next";
-import React from "react";
+import React, {Suspense} from "react";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-const CustomersPage = () => {
-  return <div>CustomersPage</div>;
+const CustomersPage = async () => {
+  const customers = await fetchFilteredCustomers("");
+  return (
+    <div className="w-full">
+      <Suspense>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default CustomersPage;
